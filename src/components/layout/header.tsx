c06475a64f766cae6a0f628978ba670d60613d69
Prashant <shashankverma426@gmail.com>
2025-09-06 14:12:10 +0000

    increse the size of this section (_for element div_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,7 +11,7 @@ import {
 export function Header() {
   return (
     <header className="bg-card shadow-sm sticky top-0 z-40">
-      <div className="container mx-auto flex items-center justify-between px-4">
+      <div className="container mx-auto flex items-center justify-between py-2 px-4">
         <div className="flex items-center gap-1">
           <Image src="/logoo.png" alt="Sehat Saathi logo" width={48} height={48} />
           <span className="text-2xl font-bold font-headline relative -top-0.5">Sehat Saathi</span>
